a32e2189696cd80a706581903f8ec9d81bebce9b
Vivi <viviane.souza05@aluno.ifce.edu.br>
2023-07-28 17:01:17 -0300

    today's studies

--- lib/main.dart
+++ lib/main.dart
@@ -34,74 +34,158 @@ class bmi_calculator extends StatelessWidget {
         ),
         backgroundColor: const Color(0xff608334),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          children: [
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Hello!",
+      // o singleChilScrollView evita aquele problema do teclado quando aberto apresentar problema de overflow, mesmo o código, nossa aplicação estando toda ok.
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            children: [
+              const Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Hello!",
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 11,
+              ),
+              const Text(
+                "Did you know that the BMI can influence your self-esteem and mental health, as well as your health and well-being? In light of this, you can see how important calculating your BMI, Body Mass Index, is for a fuller life, right? Below is a calculator so you can calculate your BMI yourself, and then you can take it to your nutritionist, fitness instructor, etc. Shall we begin? \u{1F600}",
                 style: TextStyle(
                   fontFamily: 'Roboto',
-                  fontSize: 25,
-                  fontWeight: FontWeight.w700,
+                  fontSize: 19,
+                  fontWeight: FontWeight.w500,
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 11,
-            ),
-            const Text(
-              "Did you know that the BMI can influence your self-esteem and mental health, as well as your health and well-being? In light of this, you can see how important calculating your BMI, Body Mass Index, is for a fuller life, right? Below is a calculator so you can calculate your BMI yourself, and then you can take it to your nutritionist, fitness instructor, etc. Shall we begin? \u{1F600}",
-              style: TextStyle(
-                fontFamily: 'Roboto',
-                fontSize: 19,
-                fontWeight: FontWeight.w500,
+              const SizedBox(
+                height: 40,
               ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Container(
-                width: 320,
-                height: 320,
-                decoration: BoxDecoration(
-                  color: const Color(0xffA8CA7E),
-                  borderRadius: BorderRadius.circular(35),
-                ),
-                child: Column(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(top: 18.0, left: 25),
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          "Enter your name: ",
-                          style: TextStyle(
-                            fontFamily: 'Roboto',
-                            fontSize: 19,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Container(
+                  width: 320,
+                  height: 320,
+                  decoration: BoxDecoration(
+                    color: const Color(0xffA8CA7E),
+                    borderRadius: BorderRadius.circular(35),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(top: 18.0, left: 25),
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              "Enter your name: ",
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 19,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    Container(
-                      height: 50,
-                      width: 280,
-                      decoration: BoxDecoration(
-                          color: Colors.white60,
-                          borderRadius: BorderRadius.circular(20),
+                        Container(
+                          height: 50,
+                          width: 280,
+                          decoration: BoxDecoration(
+                            color: Colors.white60,
+                            borderRadius: BorderRadius.circular(20),
                           ),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 10),
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 18.0, left: 25),
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              "Enter your body weight: ",
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 19,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 50,
+                          width: 280,
+                          decoration: BoxDecoration(
+                            color: Colors.white60,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 10),
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 18.0, left: 25),
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              "Enter your height: ",
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 19,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 50,
+                          width: 280,
+                          decoration: BoxDecoration(
+                            color: Colors.white60,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 10),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 60.0),
+                          child: Container(
+                            height: 50,
+                            width: 120,
+                            decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
